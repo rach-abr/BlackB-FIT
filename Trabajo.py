3768ac876311ddf906383f0ac6ab3b4f08e170cd
Rach <racherbas@gmail.com>
2024-06-10 18:23:19 -0300

    Initial commit

---
+++ Trabajo.py
@@ -0,0 +1,106 @@
+'''
+BlackBFIT
+Autores: 
+Fecha: 2024
+Version: 1.0
+'''
+import os 
+import colorama
+import time
+
+def limpiarPantalla():
+    os.system("cls")
+    return
+
+ejercicios = {
+    "Pecho": ["Press de banca con barra, Apertura con mancuernas"],
+    "Espalda": ["Dominadas, Remo con barra"],
+    "Brazos": ["Extencion de triceps, Biceps con mancuernas"],
+    "Hombros": ["Press militar, Elevaciones laterales"],
+    "Piernas": ["Prensa de piernas, Sentadilla con barra"],
+    "Gluteos": ["Hiptrust, Sentadilla sumo con peso"]
+}
+
+pesoKg = {
+    "Pecho": [0,0],
+    "Espalda": [0,0],
+    "Brazos": [0,0],
+    "Hombros": [0,0],
+    "Piernas": [0,0],
+    "Gluteos": [0,0]
+}
+
+def menu():
+    limpiarPantalla()
+    print(colorama.Fore.YELLOW + "BlackB FIT".center(45))
+    print("="*45)
+    print(colorama.Fore.YELLOW + "\t1" + colorama.Fore.RESET + " Pecho")
+    print(colorama.Fore.YELLOW + "\t2" + colorama.Fore.RESET + " Espalda")
+    print(colorama.Fore.YELLOW + "\t3" + colorama.Fore.RESET + " Brazos")
+    print(colorama.Fore.YELLOW + "\t4" + colorama.Fore.RESET + " Hombros")
+    print(colorama.Fore.YELLOW + "\t5" + colorama.Fore.RESET + " Piernas")
+    print(colorama.Fore.YELLOW + "\t6" + colorama.Fore.RESET + " Gluteos")
+    print(colorama.Fore.YELLOW + "\t7" + colorama.Fore.RESET + " Agregar/Modificar/Eliminar peso en KG")
+    print(colorama.Fore.YELLOW + "\t8" + colorama.Fore.RESET + " Salir")
+    op = int(input(colorama.Fore.MAGENTA + "Seleccione una zona a trabajar: ".center(45) + colorama.Fore.RESET ))
+    return op
+
+def gestionar_peso(zona):
+    print(f"\nGestión de pesos para {zona}:")
+    for x, ejercicio in enumerate(ejercicios[zona], 1):
+        print(f"{x}. {ejercicio} - Peso actual: {pesoKg[zona][x - 1]} kg")
+    opcion = int(input("Seleccione un ejercicio para modificar el peso (1-2): "))
+    accion = input("Desea agregar/modificar/eliminar el peso (a/m/e): ").lower()
+    if accion == 'a' or accion == 'm':
+        nuevo_peso = float(input("Ingrese el nuevo peso: "))
+        pesoKg[zona][opcion - 1] = nuevo_peso
+        print(f"Peso actualizado: {pesoKg[zona][opcion - 1]} kg")
+    elif accion == 'e':
+        pesoKg[zona][opcion - 1] = 0
+        print("Peso eliminado.")
+    else:
+        print("Acción no válida.")
+
+#programa principal
+colorama.init() 
+terminos=[]
+op = menu()
+while op !=9:
+    match op:
+        case 1:
+            print("agregar")
+            agregar(terminos)
+            time.sleep(5)
+            input(colorama.Fore.RED + "presione enter para continuar...")
+        case 2:
+            print("modificar")
+            modificar(terminos)
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+        case 3:
+            print("eliminar")
+            eliminar(terminos)
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+        case 4:
+            print("buscar")
+            buscar(terminos)
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+        case 5:
+            print("listar todos")
+            listar(terminos)
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+        case 6:
+            print("listar todos")
+            listar(terminos)
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+        case 7:
+            print("listar todos")
+            listar(terminos)
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+        case 8:
+            print("listar todos")
+            listar(terminos)
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+        case _:
+            print("error!")
+            input(colorama.Fore.RED + "Presione enter para continuar...")
+    op = menu()
---
+++ descripcion.md
@@ -0,0 +1,34 @@
+# BlackB FIT
+
+## Descripcion del Proyecto
+El proyecto Glosario de Términos de Programación en Python es una aplicación de consola que permite a los usuarios gestionar un glosario de términos relacionados con la programación. La aplicación ofrece funcionalidades para agregar, buscar, modificar, eliminar y listar términos, proporcionando persistencia de datos mediante el uso de archivos de texto.
+
+El objetivo principal de este proyecto es proporcionar una herramienta sencilla y eficaz para almacenar y consultar definiciones de términos de programación, facilitando el aprendizaje y la referencia rápida para estudiantes y profesionales del desarrollo de software.
+
+## Público Objetivo
+Este proyecto está dirigido a estudiantes de programación, desarrolladores y cualquier persona interesada en aprender y tener una referencia rápida de términos de programación. Es una herramienta educativa que facilita el almacenamiento y consulta de definiciones importantes en el campo del desarrollo de software.
+
+## Requerimientos Funcionales
+1. Funcionalidades Básicas
+    - Agregar Término: Permitir al usuario agregar un nuevo término junto con su definición.
+    - Buscar Término: Permitir al usuario buscar un término y mostrar su definición.
+    - Modificar Término: Permitir al usuario modificar la definición de un término existente.
+    - Eliminar Término: Permitir al usuario eliminar un término del glosario.
+    - Listar Términos: Permitir al usuario listar todos los términos disponibles en el glosario.
+
+2. Persistencia de Datos
+    - Almacenamiento en Archivos de Texto: Utilizar archivos de texto para guardar y leer los términos y sus definiciones.
+    - Formato del Archivo: Cada término y su definición se deben almacenar en un formato
+3. Interfaz de Usuario
+    - Interfaz de Consola: Proporcionar una interfaz de línea de comandos que permita al usuario interactuar con el glosario.
+    - Menú Principal: Ofrecer un menú principal desde el cual el usuario pueda seleccionar las diferentes funcionalidades (Agregar, Buscar, Modificar, Eliminar, Listar).
+4.  Validaciones de Datos
+    - Entrada de Usuario: Validar la entrada del usuario para asegurar que los términos y definiciones no estén vacíos y que los términos no se repitan.
+    - Gestión de Errores: Manejar posibles errores, como intentos de búsqueda o modificación de términos que no existen en el glosario.
+5.  Estructura del Código
+    - Modularidad: Organizar el código en funciones y, si es necesario, en clases, para mejorar la legibilidad y mantenibilidad.
+    - Funciones Principales: Crear funciones específicas para agregar, buscar, modificar, eliminar y listar términos.
+    - Manejo de Archivos: Crear funciones para leer y escribir en los archivos de texto.
+6.  Documentación
+    - Comentarios en el Código: Incluir comentarios en el código para explicar el propósito y funcionamiento de las diferentes partes.
+    - Archivo README: Proporcionar un archivo README que describa el propósito del proyecto, cómo ejecutarlo y cómo utilizar las diferentes funcionalidades.
